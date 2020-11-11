f068bce06c61a3393c59abf5ef0b23a1ca3dd59d
Arthur Vilela Frazão <59981437+ArthurFrazao@users.noreply.github.com>
2020-11-11 13:14:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from botinstagram import InstagramBot
 import getpass, time
 
+""" Login Instagram """
 usuario = input("Nome de usuario: ")
 senha = getpass.getpass('Senha: ')
 perfil = input("Pesquisar perfil: ")
 
+""" Usuario e Senha do instagram utilizados para fazer login na conta """
 instaBot = InstagramBot(usuario, senha)
 instaBot.maximizar_janela()
 instaBot.login()
+
+""" Perfil que deseja pesquisar """
 instaBot.abrir_perfil(perfil)
 instaBot.curtir_sem_parar()
 instaBot.fechar()
