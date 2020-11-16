6fb19559eef491bf896fafafbb2753d94d432129
Arthur Vilela Frazão <59981437+ArthurFrazao@users.noreply.github.com>
2020-11-16 15:07:59 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from botinstagram import InstagramBot
 import getpass, time
 
-usuario = input("Nome de usuario: ")
-senha = getpass.getpass('Senha: ')
-perfil = input("Pesquisar perfil: ")
+"""próximas atualizações:
+- incluir no bot opções de seguir perfil e de pesquisar mais de 1 perfil"""
 
-instaBot = InstagramBot(usuario, senha)
-instaBot.maximizar_janela()
-instaBot.login()
-instaBot.abrir_perfil(perfil)
-instaBot.curtir_sem_parar()
-instaBot.fechar()
+def main():
+    usuario = input("Nome de usuario: ")
+    senha = getpass.getpass("Senha: ")   
+    perfil = input("Pesquisar perfil: ")
+    instaBot = InstagramBot(usuario, senha)
+    instaBot.maximizar_janela()
+    instaBot.login()
+    instaBot.abrir_perfil(perfil)
+    instaBot.curtir_sem_parar()
+    instaBot.fechar()
+    
+if __name__ == "__main__":
+    main()
