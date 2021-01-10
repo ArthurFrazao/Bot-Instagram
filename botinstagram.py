08dad5eecb2f5db05aa87e18b085010c577214cc
Arthur Vilela Frazão <59981437+ArthurFrazao@users.noreply.github.com>
2021-01-10 18:08:03 -0300

    update code

--- botinstagram.py
+++ botinstagram.py
@@ -5,108 +5,157 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as bs
-import time
+import time, getpass
+
+driver = webdriver.Firefox(executable_path=r'C:\Users\arthu\Documents\Projetos\BotInstagram\geckodriver\geckodriver.exe')
 
 class InstagramBot:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-        self.driver = webdriver.Firefox(executable_path=r'C:\Users\arthu\OneDrive\Área de Trabalho\geckodriver\geckodriver.exe')
+    def __init__(self):
+        self.driver = driver  
+        self.maximize_window()      
+        self.login()        
 
     def login(self):  
+        user = input("Nome de usuario: ")   
+        password = getpass.getpass("Senha: ") 
         self.driver.get('https://www.instagram.com/')
         time.sleep(2)
 
-        '''Inserido usuário'''
-        self.driver.find_element_by_xpath("//input[@name='username']").send_keys(self.username)
+        #insert username             
+        self.driver.find_element_by_xpath("//input[@name='username']").send_keys(user)
+        
+        #insert password          
+        self.driver.find_element_by_xpath("//input[@name='password']").send_keys(password)
 
-        '''inserindo senha'''
-        self.driver.find_element_by_xpath("//input[@name='password']").send_keys(self.password)
-
-        '''submetendo dados e confirmando login'''
+        #submit login
         submit = self.driver.find_element_by_tag_name('form')
         submit.submit()
 
-        try:
-            """Fechando as notificações"""
-
-            """Primeira etapa de confirmação"""
+        #close notification
+        try:            
+            #1st pop-up            
             notifications = WebDriverWait(self.driver, 8).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Agora não"]')))
             notifications.click()
 
-            """Segunda etapa de confirmação"""
+            #2nd pop-up
             WebDriverWait(self.driver, 2).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Agora não"]'))).click()
         except NoSuchElementException:
             pass
-    
-    def abrir_perfil(self, usuario):
+        
+    def open_profile(self):
+        profile = input("Perfil: ")
         try:
-            self.driver.get('https://www.instagram.com/' + usuario + '/')
+            self.driver.get('https://www.instagram.com/' + profile + '/')
             time.sleep(1.5)
         except NoSuchElementException:
             print("Falha na busca")
 
-    def abrir_primeira_foto(self):
+    def open_1st_pic(self):
         try:
             self.driver.find_element_by_xpath("//div[@class=\"eLAPa\"]").click()
-            time.sleep(4)
+            time.sleep(2)
         except NoSuchElementException:
-            print("O Usuário não possui foto ou você não tem permissão.") 
+            print("O Usuário não possui foto(s) ou você não tem permissão.") 
 
-    def proxima_foto(self):    
-        botao_proximo = "//a[text()=\"Próximo\"]"        
+    def next_pic(self):    
+        button_next = "//a[text()=\"Próximo\"]"        
         try:
-            self.driver.find_element_by_xpath(botao_proximo).click()
+            self.driver.find_element_by_xpath(button_next).click()
             time.sleep(3)
             return True
         except NoSuchElementException:
             try:
-                self.curtir_foto()
+                self.like()
             except:
                 pass
             print("- Usuário não possui mais fotos seguintes.")
             return False
 
-    def curtir_sem_parar(self):
-        self.abrir_primeira_foto()
-        self.curtir_foto()
+    def like_unstoppable(self):        
+        # Curte fotos até o final do perfil ou interrupção
+        
+        self.open_1st_pic()
+        self.like()
         while(True):
-            prox_post = self.proxima_foto()
+            hasProxPost = self.next_pic()
     
-            if prox_post == True:
+            # Se tiver botão de próxima foto -> esperado TRUE
+            if hasProxPost == True:
     
-                """CURTIR FOTO"""
-                self.curtir_foto()
+                # Curtir
+                self.like()
                 time.sleep(2)
 
-                """BOTÃO PRÓXIMO"""
-                self.proxima_foto()
+                # Botão próximo
+                self.next_pic()
                 time.sleep(2)    
-                
+            
+            # Se não tiver botão de próxima foto    
             else:
                 print("- Não há mais fotos disponíveis para serem curtidas.")
                 break
 
-    def curtir_foto(self):
-        curtir = self.driver.find_element_by_class_name('fr66n')
-        soup = bs(curtir.get_attribute('innerHTML'),'html.parser')
+    def like(self):
+        button_like = self.driver.find_element_by_class_name('fr66n')
+        soup = bs(button_like.get_attribute('innerHTML'),'html.parser')
+        
         if(soup.find('svg')['aria-label'] == 'Curtir'):
-            curtir.click()
+            button_like.click()
         time.sleep(2)
 
-    def fechar(self):
+    def like_photo_by_quantity(self):
+        quantity = int(input("Quantas fotos deseja curtir? "))
+        
+        self.open_1st_pic()
+        self.like()
+        i = 1
+        while(i < quantity):
+            self.next_pic()
+            self.like()
+            i+=1
+        
+    def follow(self):
+        try:            
+            self.driver.find_element_by_xpath('//button[text()="Seguir"]').click()
+        except:
+            pass    
+     
+    def open_direct(self):
+        try:
+            WebDriverWait(self.driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'xWeGp'))).click()
+        except:
+            pass
+        
+    def send_message(self):
+        self.open_direct()
+        
+        messageforwho = input("Enviar mensagem para: ")
+        message = input("Mensagem: ")
+    
+        try:
+            WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Enviar mensagem"]'))).click()                                    
+        except:
+            pass
+        
+        self.driver.find_element_by_xpath('//input[@name="queryBox"]').send_keys(messageforwho)
+        time.sleep(4)
+        
+        # Selecionar o radio do destinatario
+        WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".dCJp8:nth-of-type(1)"))).click()
+        
+        # Botão de confirmar destinatario
+        WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.CLASS_NAME, "rIacr"))).click()
+        
+        # Enviar mensagem ao textarea
+        WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea'))).send_keys(message) 
+        
+        # Botão enviar mensagem
+        WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.XPATH, '//button[text()="Enviar"]'))).click()   
+    
+    def close_window(self):
+        print("Fechando webdriver...")
         time.sleep(4)
         self.driver.quit()
 
-    def maximizar_janela(self):
+    def maximize_window(self):
         self.driver.maximize_window()
-
-    def curtir_foto_by_qntd(self, qntd):
-        self.abrir_primeira_foto()
-        self.curtir_foto()
-        i = 1
-        while(i < qntd):
-            self.proxima_foto()
-            time.sleep(2)
-            self.curtir_foto()
-            i+=1      
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from botinstagram import InstagramBot
-import getpass, time
 
-"""próximas atualizações:
-- incluir no bot opções de seguir perfil e de pesquisar mais de 1 perfil"""
-
-def main():
-    usuario = input("Nome de usuario: ")
-    senha = getpass.getpass("Senha: ")   
-    perfil = input("Pesquisar perfil: ")
-    instaBot = InstagramBot(usuario, senha)
-    instaBot.maximizar_janela()
-    instaBot.login()
-    instaBot.abrir_perfil(perfil)
-    instaBot.curtir_sem_parar()
-    instaBot.fechar()
-    
+def main():    
+    instaBot = InstagramBot()
+        
 if __name__ == "__main__":
     main()
